7dd85387a14cfcab839f8255a184ae28b38902f5
Édes Balázs <edesbalazs@gmail.com>
2017-03-28 21:59:28 +0100

    switched tape with jest

--- test.js
+++ test.js
@@ -1,211 +1,212 @@
 'use strict'
 
-const test = require('tape')
 const mewt = require('./lib')
 
-const createAssertMewt = t => input => {
-  t.equal(typeof input.$set, 'function')
-  t.equal(typeof input.$unset, 'function')
+const assertMewt = input => {
+  expect(typeof input.$set).toBe('function')
+  expect(typeof input.$unset).toBe('function')
 }
 
-test('exports a function', (t) => {
-  t.equal(typeof mewt, 'function')
-  t.end()
-})
+describe('mewt', () => {
+  const assertMewt = input => {
+    expect(typeof input.$set).toBe('function')
+    expect(typeof input.$unset).toBe('function')
+  }
 
-test('throws without object or array', (t) => {
-  t.throws(() => mewt(), /accepts array or object/)
-  t.end()
-})
+  describe('generic', () => {
+    it('should export a function', () => {
+      expect(typeof mewt).toBe('function')
+    })
 
-test('array', (t) => {
-  const assertMewt = createAssertMewt(t)
-  {
-    // returns new array
-    const a = []
-    const n = mewt(a)
-    t.notEqual(a, n)
-  }
-  {
-    // throws on mutation
-    const a = mewt([])
-    t.throws(() => a[0] = 'Lodger', /immutable/)
-  }
-  {
-    // has $set & $unset
-    const a = mewt([])
-    assertMewt(a)
-  }
-  {
-    // get own property
-    const a = mewt([''])
-    t.equal(a[0], '')
-  }
-  {
-    // $set
-    const a = mewt([])
-    const n = a.$set(0, '')
-    t.deepEqual(n, [''])
-    t.notEqual(a, n)
-    assertMewt(n)
-  }
-  {
-    // $unset
-    const a = mewt([''])
-    const n = a.$unset(0)
-    t.deepEqual(n, [])
-    t.notEqual(a, n)
-    assertMewt(n)
-  }
-  {
-    // copyWithin
-    const a = mewt([1, 2])
-    const n = a.copyWithin(0, 2)
-    t.deepEqual(n, [1, 2])
-    t.notEqual(a, n)
-    assertMewt(n)
-  }
-  {
-    // fill
-    const a = mewt([,])
-    const n = a.fill('')
-    t.deepEqual(n, [''])
-    t.notEqual(a, n)
-    assertMewt(n)
-  }
-  {
-    // pop
-    const a = mewt([''])
-    const [str, n] = a.pop()
-    t.deepEqual(a, [''])
-    t.equal(str, '')
-    t.deepEqual(n, [])
-    t.notEqual(a, n)
-    assertMewt(n)
-  }
-  {
-    // push
-    const a = mewt([])
-    const [len, n] = a.push('')
-    t.equal(1, len)
-    t.deepEqual(n, [''])
-    t.notEqual(a, n)
-    assertMewt(n)
-  }
-  {
-    // reverse
-    const a = mewt([1, 2])
-    const n = a.reverse()
-    t.deepEqual(n, [2, 1])
-    t.notEqual(a, n)
-    assertMewt(n)
-  }
-  {
-    // shift
-    const a = mewt([1, 2])
-    const [num, n] = a.shift()
-    t.equal(num, 1)
-    t.deepEqual(n, [2])
-    t.notEqual(a, n)
-    assertMewt(n)
-  }
-  {
-    // sort
-    const a = mewt(['c', 'b', 'a'])
-    const n = a.sort()
-    t.deepEqual(n, ['a', 'b', 'c'])
-    t.notEqual(a, n)
-    assertMewt(n)
-  }
-  {
-    // sort
-    const a = mewt(['a', 'b', 'c'])
-    const n = a.splice(0, 1)
-    t.deepEqual(n, ['a'])
-    t.notEqual(a, n)
-    assertMewt(n)
-  }
-  {
-    // unshift
-    const a = mewt([2])
-    const [len, n] = a.unshift(1)
-    t.equal(len, 2)
-    t.deepEqual(n, [1, 2])
-    t.notEqual(a, n)
-    assertMewt(n)
-  }
-  {
-    // filter
-    const a = mewt([1, 2, 3, 4, 5])
-    const filtered = a.filter(e => e % 2 === 0)
-    t.deepEqual(filtered, [2, 4])
-    assertMewt(filtered)
-  }
-  {
-    // map
-    const a = mewt([1, 2, 3, 4, 5])
-    const transformed = a.map(e => e * 2)
-    t.deepEqual(transformed, [2, 4, 6, 8, 10])
-    assertMewt(transformed)
-  }
-  {
-    // concat
-    const a = mewt([1, 2]).concat([3, 4, 5])
-    t.deepEqual(a, [1, 2, 3, 4, 5])
-    assertMewt(a)
+    it('should throw without object or array', () => {
+      expect(() => mewt()).toThrowError(/accepts array or object/)
+    })
+  })
 
-    const b = mewt([5, 4]).concat(mewt([3, 2, 1]))
-    t.deepEqual(b, [5, 4, 3, 2, 1])
-    assertMewt(b)
-  }
-  {
-    // slice
-    const a = mewt([1, 2, 3, 4, 5])
-    const sliced = a.slice(1, 4)
-    t.deepEqual(sliced, [2, 3, 4])
-    assertMewt(sliced)
-  }
-  t.end()
-})
+  describe('array', () => {
+    it('should return a new array', () => {
+      const a = []
+      const n = mewt(a)
+      expect(n).not.toBe(a)
+      expect(n).toEqual(a)
+    })
 
-test('object', (t) => {
-  const assertMewt = createAssertMewt(t)
-  {
-    // returns new object
-    const o = {}
-    const n = mewt(o)
-    t.notEqual(o, n)
-  }
-  {
-    // throws on mutation
-    const o = mewt({})
-    t.throws(() => o.track = 'The Promise', /immutable/)
-  }
-  {
-    // has $set & $unset
-    const o = mewt({})
-    assertMewt(o)
-  }
-  {
-    // get own property
-    const o = mewt({ album: 'Aladdin Sane' })
-    t.equal(o.album, 'Aladdin Sane')
-  }
-  {
-    // $set
-    const o = mewt({})
-    const n = o.$set('album', 'Hours')
-    t.equal(o.album, undefined)
-    t.equal(n.album, 'Hours')
-    t.notEqual(o, n)
-  }
-  {
-    // $unset
-    const o = mewt({ album: 'Heroes' })
-    const n = o.$unset('album')
-    t.equal(o.album, 'Heroes')
-    t.equal(n.album, undefined)
-    t.notEqual(o, n)
-  }
-  t.end()
+    it('should throw on mutation', () => {
+      const a = mewt([])
+      expect(() => a[0] = 'Lodger').toThrowError(/immutable/)
+    })
+
+    it('should have $set & $unset', () => {
+      const a = mewt([])
+      assertMewt(a)
+    })
+
+    it('should get own property', () => {
+      const a = mewt([''])
+      expect(a[0]).toBe('')
+    })
+
+    it('should properly $set at index', () => {
+      const a = mewt([])
+      const n = a.$set(0, '')
+      expect(n).toEqual([''])
+      expect(a).not.toBe(n)
+      assertMewt(n)
+    })
+
+    it('should properly $unset at index', () => {
+      const a = mewt([''])
+      const n = a.$unset(0)
+      expect(n).toEqual([])
+      expect(a).not.toBe(n)
+      assertMewt(n)
+    })
+
+    it('should copyWithin without mutation', () => {
+      const a = mewt([1, 2])
+      const n = a.copyWithin(0, 2)
+      expect(n).toEqual([1, 2])
+      expect(n).not.toBe(a)
+      assertMewt(n)
+    })
+
+    it('should fill without mutation', () => {
+      const a = mewt([,])
+      const n = a.fill('')
+      expect(n).toEqual([''])
+      expect(a).not.toBe(n)
+      assertMewt(n)
+    })
+
+    it('should pop without mutation', () => {
+      const a = mewt([''])
+      const [str, n] = a.pop()
+      expect(a).toEqual([''])
+      expect(str).toBe('')
+      expect(n).toEqual([])
+      expect(a).not.toBe(n)
+      assertMewt(n)
+    })
+
+    it('should push without mutation', () => {
+      const a = mewt([])
+      const [len, n] = a.push('')
+      expect(len).toBe(1)
+      expect(n).toEqual([''])
+      expect(a).not.toBe(n)
+      assertMewt(n)
+    })
+
+    it('should reverse without mutation', () => {
+      const a = mewt([1, 2])
+      const n = a.reverse()
+      expect(n).toEqual([2, 1])
+      expect(a).not.toBe(n)
+      assertMewt(n)
+    })
+
+    it('should shift without mutation', () => {
+      const a = mewt([1, 2])
+      const [num, n] = a.shift()
+      expect(num).toBe(1)
+      expect(n).toEqual([2])
+      expect(a).not.toBe(n)
+      assertMewt(n)
+    })
+
+    it('should sort without mutation', () => {
+      const a = mewt(['c', 'b', 'a'])
+      const n = a.sort()
+      expect(n).toEqual(['a', 'b', 'c'])
+      expect(a).not.toBe(n)
+      assertMewt(n)
+    })
+
+    it('should splice without mutation', () => {
+      const a = mewt(['a', 'b', 'c'])
+      const n = a.splice(0, 1)
+      expect(n).toEqual(['a'])
+      expect(a).not.toBe(n)
+      assertMewt(n)
+    })
+
+    it('should unshift without mutation', () => {
+      const a = mewt([2])
+      const [len, n] = a.unshift(1)
+      expect(len).toBe(2)
+      expect(n).toEqual([1, 2])
+      expect(a).not.toBe(n)
+      assertMewt(n)
+    })
+
+    it('should filter and the new array should be a mewt', () => {
+      const a = mewt([1, 2, 3, 4, 5])
+      const filtered = a.filter(e => e % 2 === 0)
+      expect(filtered).toEqual([2, 4])
+      assertMewt(filtered)
+    })
+
+    it('should transform (map) and the new array should be a mewt', () => {
+      const a = mewt([1, 2, 3, 4, 5])
+      const transformed = a.map(e => e * 2)
+      expect(transformed).toEqual([2, 4, 6, 8, 10])
+      assertMewt(transformed)
+    })
+
+    it('should concat and the new array should be a mewt', () => {
+      const a = mewt([1, 2]).concat([3, 4, 5])
+      expect(a).toEqual([1, 2, 3, 4, 5])
+      assertMewt(a)
+
+      const b = mewt([5, 4]).concat(mewt([3, 2, 1]))
+      expect(b).toEqual([5, 4, 3, 2, 1])
+      assertMewt(b)
+    })
+
+    it('should slice and the new array should be a mewt', () => {
+      const a = mewt([1, 2, 3, 4, 5])
+      const sliced = a.slice(1, 4)
+      expect(sliced).toEqual([2, 3, 4])
+      assertMewt(sliced)
+    })
+  })
+
+  describe('object', () => {
+    it('should return a new object', () => {
+      const o = {}
+      const n = mewt(o)
+      expect(o).not.toBe(n)
+    })
+
+    it('should throw on mutation', () => {
+      const o = mewt({})
+      expect(() => o.track = 'The Promise').toThrowError(/immutable/)
+    })
+
+    it('should have $set & $unset', () => {
+      const o = mewt({})
+      assertMewt(o)
+    })
+
+    it('should get own property', () => {
+      const o = mewt({ album: 'Aladdin Sane' })
+      expect(o.album).toBe('Aladdin Sane')
+    })
+
+    it('should properly $set at key', () => {
+      const o = mewt({})
+      const n = o.$set('album', 'Hours')
+      expect(o.album).toBeUndefined()
+      expect(n.album).toBe('Hours')
+      expect(o).not.toBe(n)
+    })
+
+    it('should properly $unset at key', () => {
+      const o = mewt({ album: 'Heroes' })
+      const n = o.$unset('album')
+      expect(o.album).toBe('Heroes')
+      expect(n.album).toBeUndefined()
+      expect(o).not.toBe(n)
+    })
+  })
 })
